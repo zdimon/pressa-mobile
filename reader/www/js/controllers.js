2ad9afe7b77aa6ef87955218619bd98f2ad64a60
zdimon <zdimon77@gmail.com>
2016-03-24 11:35:07 +0200

    auto commit

--- reader/www/js/controllers.js
+++ reader/www/js/controllers.js
@@ -73,7 +73,7 @@ angular.module('starter.controllers', [])
 
     element = angular.element(document.querySelector('#eventCiao'));
     //$ionicGesture.on('on-tap', function(){ alert('sss'); }, element);
-
+       $scope.zoom = 30;
        $ionicGesture.on('on-swipe', function(e)
         {
             $scope.zoom = e.gesture.scale;
